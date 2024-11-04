d2b2005f875c82c27cc699f84f325b4307f06295
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-04 08:08:32 -0600

    Model of DataBase Firebase

---
+++ src/models/empleadoModel.py
@@ -0,0 +1,13 @@
+# Definir la clase de atributos que contiene el empleado
+class Empleado:
+  def __init__(self, nombre, apaterno, amaterno, direccion, telefono, ciudad, estado, usuario, password, rol):
+    self.nomnre = nombre
+    self.apataerno = apaterno
+    self.amaterno = amaterno
+    self.direccion = direccion
+    self.telefono = telefono
+    self.ciudad = ciudad
+    self.estaro = estado
+    self.usuario = usuario
+    self.password = password
+    self.rol = rol
