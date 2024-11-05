b06c25f737bdb358761386dfdc1d26cec3416662
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-04 19:56:04 -0600

    Constructor of DataBase Collection

--- src/models/empleadoModel.py
+++ src/models/empleadoModel.py
@@ -1,13 +1,22 @@
 # Definir la clase de atributos que contiene el empleado
 class Empleado:
-  def __init__(self, nombre, apaterno, amaterno, direccion, telefono, ciudad, estado, usuario, password, rol):
-    self.nomnre = nombre
-    self.apataerno = apaterno
+  def __init__(self, id=None, nombre='', apaterno='', amaterno='', direccion='', telefono='', ciudad='', estado='', usuario='', password='', rol=''):
+    self.id = id,
+    self.nombre = nombre
+    self.apaterno = apaterno
     self.amaterno = amaterno
     self.direccion = direccion
     self.telefono = telefono
     self.ciudad = ciudad
-    self.estaro = estado
+    self.estado = estado
     self.usuario = usuario
     self.password = password
     self.rol = rol
+    
+  # Interaccion entre Proyecto y Firebase, mandarlo a la DB y que se guarde
+  def to_dict(self):
+    return self.__dict__
+  
+  @staticmethod 
+  def from_dict(data): # Como regregamos el Empleado 
+    return Empleado(**data)
