1080efca74a552d177e1facfd48e6e685a7726a7
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-04 19:58:25 -0600

    File that works with the model

--- src/repositories/empleadoRepository.py
+++ src/repositories/empleadoRepository.py
@@ -1,27 +1,40 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+from config.firebaseConfig import initialize_firebase
 from models.empleadoModel import Empleado
 
-cred = credentials.Certificate("RURA_AARCHIVO")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-collection = 'empleados_python'
+# cred = credentials.Certificate("RURA_AARCHIVO") -> Ya no se necesita porque tenemos archivo de Conf propio
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+# collection = 'empleados_python'
 
 class EmpleadoRepository:
-  @staticmethod
-  def get_all():
-    empleados = db.collection(collection)
-    return [doc.to_dict() for doc in empleados.stream() ] # stream => async/await primitivos de alto nivel para trabajar con conexiones de red.
+  def __init__(self):
+    self.db = initialize_firebase()
+    self.collection = self.db.collection('empleados_python')
+    
+  def get_all(self):
+    empleados = [Empleado.from_dict(doc.to_dict()) for doc in self.collection.stream()] # stream => async/await primitivos de alto nivel para trabajar con conexiones de red.
+    return empleados 
   # los streams permiten enviar y recibir datos sin utilizar callbacks y tranportes de bajo nivel.
   
-  @staticmethod
-  def create_empleado(data):
-    db.collection(collection).add(data)
+  def create_empleado(self, empleado_nuevo):
+    doc = self.collection.document()
+    doc.set(empleado_nuevo.to_dict())
     
-  @staticmethod
-  def get_by_user(username):
-    docs = db.collection.where(collection).where("usuario", "==", username).stream()
+    return doc.id
+    
+  def get_by_user(self, username):
+    docs = self.collection.where("usuario", "==", username).stream()
     
     for doc in docs:
       return Empleado.from_dict(doc.to_dict())
     return None
+  
+  def get_by_id(self, id):
+    doc = self.collection.document(id).get() # document se refiere 
+    return Empleado.from_dict(doc.to_dict()) if doc.exists else None
+  
+  def update_empleado(self, id, data):
+    self.collection.document(id).update(data)
+    
+  def delete_user(self, id):
+    self.collection.document(id).delete()
