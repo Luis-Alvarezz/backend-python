8b19515c16cf5e22f4fa87d6f6df3272319e031a
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-04 20:01:14 -0600

    Constructor for control service and work with repository

--- src/services/empleadoService.py
+++ src/services/empleadoService.py
@@ -1,28 +1,39 @@
-# Requeriremos lo del Repositorio:
-from repositories.empleado_repository import empleadoRepository
-from utils.encryption_util import encrypt_password
-from utils.token_util import generate_token
-from utils.token_util import generate_token
-import time
+import bcrypt
+from repositories.empleadoRepository import EmpleadoRepository 
+from models.empleadoModel import Empleado
+
 
 class EmpleadoService:
-  @staticmethod
-  def get_all():
-    return empleadoRepository.get_all()
+  def __init__(self):
+    self.repository = EmpleadoRepository()
   
-  @staticmethod
-  def create_empleado(data):
-    if empleadoRepository.get_by_user(data['usuario']):
+  
+  def get_all(self):
+    return  self.repository.get_all()
+  
+  def create_empleado(self, data):
+    if  self.repository.get_by_user(data['usuario']):
       return { "error": "Usuario YA existe" }
     
-    data['password'] = encrypt_password(data['password'])
-    empleadoRepository.create_empleado(data)
-    token = generate_token(data['usuario'])
-    return { 
-            "message": "Empleado Registrado",
-            "token": token
-           }
+    hashed = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    data['password'] = hashed.decode('utf-8')
+    
+    empleadoNuevo = Empleado(**data)
+    return self.repository.create_empleado(empleadoNuevo)
   
-  @staticmethod
-  def get_by_id():
-    return empleadoRepository.get_ 
+  
+  def get_by_id(self, id):
+    return self.repository.get_by_id(id) 
+  
+  def get_by_user(self, user):
+    return self.repository.get_by_user(user)
+  
+  def delete_user(self, id):
+    return self.repository.delete_user(id)
+  
+  def update_empleado(self, id, data):
+    if 'password' in data:
+      hashed = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    data['password'] = hashed.decode('utf-8')
+    
+    self.repository.update_empleado(id, data) 
