9c98fe6a528952cca96fd1d154c26e3ac3c2ccf9
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-04 20:02:14 -0600

    Main File

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
-# Importar las cosas que vamos a utilizar:
-from routes.empleados_routes import init_empleado_routes
-from middleware.auth_middleware import require_auth
 from flask import Flask
+from routes.routes import routes
 
 app = Flask(__name__) # Creamos instancia de la clase, siendo __name__ un atajo
 # Siendo necesario para que Flask sepa enn donde buscar recursos como plantaillas y archivos estátivos.
-app.config['SECRET_KEY'] = 'VAR_KEY_SECRET'
+# app.config['SECRET_KEY'] = 'VAR_KEY_SECRET' - Se puso en Middleware
+app.register_blueprint(routes)
 
-# Inicializmos las rutas 
-init_empleado_routes(app)
 
-#Definimos el middleware
-app.before_request(require_auth)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
   app.run(debug=True)
